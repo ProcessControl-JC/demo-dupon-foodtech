5e78157286ea06c1b925c5c760b1b6616c37c475
Daniel Sardá <daniel.sarda@processcontrol.es>
2026-03-17 18:01:04 +0100

    i18n(es): traducción de campos al castellano

--- solidedge_bom_import/models/product_template.py
+++ solidedge_bom_import/models/product_template.py
@@ -5,10 +5,10 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     x_solidedge_file = fields.Char(
-        string="SolidEdge Filename",
-        help="CAD source filename for traceability back to SolidEdge",
+        string="Archivo SolidEdge",
+        help="Nombre del archivo CAD de origen para trazabilidad con SolidEdge",
     )
     x_solidedge_asunto = fields.Char(
-        string="SolidEdge Asunto",
-        help="Subject/reference code from SolidEdge BOM export",
+        string="Asunto SolidEdge",
+        help="Código de referencia/asunto del archivo exportado desde SolidEdge",
     )
