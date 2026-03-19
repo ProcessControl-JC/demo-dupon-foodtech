b88b3176cde5ac922f0e880f02a80c43e856de3b
Daniel Sardá <daniel.sarda@processcontrol.es>
2026-03-19 17:13:12 +0100

    feat: add pc_mrp_bom_grouping - group planned orders by subassembly

---
+++ pc_mrp_bom_grouping/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "MRP Planned Orders - Group by Subassembly",
+    "version": "19.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Process Control",
+    "summary": "Adds parent subassembly field to MRP planned orders for grouping",
+    "depends": ["mrp_multi_level"],
+    "data": [
+        "views/mrp_planned_order_views.xml",
+    ],
+    "installable": True,
+}
